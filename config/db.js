ae2368635d2e8fa97833d1f53629924ec2414b4d
liron <LironAtar94@gmail.com>
2019-10-02 12:33:22 +0300

    make it better

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://Liron:5327158@dfusreuven-zvbh0.mongodb.net/test?retryWrites=true&w=majority'}
+    module.exports = {mongoURI: 'mongodb+srv://Liron:5327158@dfusreuven-3resq.mongodb.net/test?retryWrites=true&w=majority'}
 } else{
     console.log('Connected Locally');
     module.exports= {mongoURI: 'mongodb+srv://Liron:5327158@dfusreuven-3resq.mongodb.net/test?retryWrites=true&w=majority'}

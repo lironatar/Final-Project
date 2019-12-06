066a10c60383c68bffcb7f7770eba1f14496716d
liron <LironAtar94@gmail.com>
2019-12-06 13:57:25 +0200

    make it better

---
+++ config/db.js
@@ -0,0 +1,6 @@
+if (process.env.NODE_ENV === 'production'){
+    module.exports = {mongoURI: 'mongodb+srv://Liron:5327158@dfusreuven-3resq.mongodb.net/test?retryWrites=true&w=majority'}
+} else{
+    console.log('Connected Locally');
+    module.exports= {mongoURI: 'mongodb://localhost:27017/DfusReuven'}
+}

fd922e62a0429e4c2694dd8105d6a44e09699071
liron <LironAtar94@gmail.com>
2019-12-11 23:05:03 +0200

    fix

--- config/db.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === "production"){
-    module.exports = {mongoURI:
-    "mongodb+srv://Liron:<5327158li>@dfusreuven-3resq.mongodb.net/test?retryWrites=true&w=majority"
-    }
-}else{
-    module.exports = {mongoURI: "mongodb://localhost/DfusReuven"}
-}

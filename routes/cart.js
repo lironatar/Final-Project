d871220ced4b51ed4f1c40dca78b83ffa8f9c876
liron <LironAtar94@gmail.com>
2019-12-23 13:59:34 +0200

    Cart

--- routes/cart.js
+++ routes/cart.js
@@ -8,34 +8,39 @@ const globalHelper = require('../helper/globalHelpers');
 router.get('', async(req,res) => {
     let cart = req.session.cart || {};
     let newCart = req.session.newCart = [];
-    newCart.push(cart);
-    var finalPrice= 0;
-    let priceArray =[];
-    for (let i=0; i<newCart.length; i++){
-        priceArray.push(newCart[i].price);
-    }
-    for (let i=0; i<priceArray.length; i++){
-        finalPrice += priceArray[i];
-        
-    }
-    if (req.user != undefined){
-        finalPrice*=0.95;
-    }
-    //update cart
-    newCart.finalPrice = await finalPrice;
-    req.session.cart.finalPrice= await finalPrice ;
-    //Convert to string
-    req.session.cart.finalPrice = await req.session.cart.finalPrice.toString();
-    req.session.cart.price = await req.session.cart.price.toString();
+    if (req.session.cart){
+        newCart.push(cart);
+        var finalPrice= 0;
+        let priceArray =[];
+        for (let i=0; i<newCart.length; i++){
+            priceArray.push(newCart[i].price);
+        }
+        for (let i=0; i<priceArray.length; i++){
+            finalPrice += priceArray[i];
+            
+        }
+        if (req.user != undefined){
+            finalPrice*=0.95;
+        }
+        //update cart
+        req.session.newCart.finalPrice = finalPrice;
+        req.session.cart.finalPrice= finalPrice;
+        //Convert to string
+        req.session.cart.finalPrice = await req.session.cart.finalPrice.toString();
+        req.session.cart.price = await req.session.cart.price.toString();
 
-    console.log('#############final price: ',cart);
+        console.log('#############final price: ',cart);
+    }
+    console.log(finalPrice);
     let footer = await globalHelper.Footer();
     res.render('cart/index', {cart: newCart, category: footer, helpers:{
-        cartIsEmpty: function(){
-            if (Number.isNaN(finalPrice)){
+        cartIsFull: function(){
+            if (finalPrice == undefined){
                 return false;
             }
-            else{
+        },
+        cartIsEmpty : function(){
+            if(finalPrice == undefined){
                 return true;
             }
         },
@@ -56,11 +61,12 @@ router.get('', async(req,res) => {
 })
 
 //Paypal Route
+const paypalConfig= require('../config/paypal.js');
 
 paypal.configure({
-    'mode': 'sandbox', //sandbox or live
-    'client_id': 'AexEfkr_JlNAiKiuzaBcd22q2i6lzg8rAfv1nPbG8qkRSQ6PM8JWfuMjFCR8hgcjyhmBIpb-1TY5e4nz',
-    'client_secret': 'EHwUE3DSj6cXeVRVFadLBDaeDsqyA0VDlyzIpO6SgfB4SjQgVsn9p-ie5wDi8l9GqfZgM4P4XvhdFYKQ'
+    'mode': paypalConfig.mode, //sandbox or live
+    'client_id': paypalConfig.client_id,
+    'client_secret': paypalConfig.client_secret
 });
 router.post('/paypal', async(req,res)=>{
     let cart = await req.session.cart;
@@ -71,8 +77,8 @@ router.post('/paypal', async(req,res)=>{
         "payment_method": "paypal"
     },
     "redirect_urls": {
-        "return_url":"http://localhost:5000/cart/paypal/success", //"http://localhost:5000",
-        "cancel_url": "http://localhost:5000"
+        "return_url":paypalConfig.return_url, //"http://localhost:5000",
+        "cancel_url": paypalConfig.cancel_url
     },
     "transactions": [{
         "item_list": {

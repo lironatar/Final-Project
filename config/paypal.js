36e7e188360b27de76ff6e23b7c1f6fe230dc65e
liron <LironAtar94@gmail.com>
2019-12-23 14:02:16 +0200

    Cart

--- config/paypal.js
+++
@@ -1,7 +0,0 @@
-
-exports.mode ='sandbox';
-exports.client_id ='AZuiUxz7WURmhQrBYg_unb76t5kzb3qIiOcP43aN0s93EayoeHVxoim_iWld-wrfy45OpMR1S9V_Ny_3';
-exports.client_secret ='EEHmGOjNmpwK0164a4Nm4gVMYUoytEhUcTy7peNJiJFdeb-N0WBNSsD68LpX4zDfHvNKY7WLwgawKdhM';
-
-exports.return_url = 'http://localhost:5000/cart/paypal/success';
-exports.cancel_url = 'http://localhost:5000';
